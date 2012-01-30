1771d4ff92009a7e9ae237ac5ff671e7ea111992
Li Xiao <swing1979@gmail.com>
2012-01-30 14:24:11 -0800

    fixed test broken because of tests depending on same test class in ObjectSpace could not be cleaned up

--- test/kestrel_queue_test.rb
+++ test/kestrel_queue_test.rb
@@ -3,28 +3,25 @@ require "qutest"
 
 class KestrelQueueTest < Test::Unit::TestCase
   def setup
-    GC.start
     Qutest.kestrel = KestrelStub.new
   end
 
-  def teardown
-    Object.module_eval do
-      remove_const(:SimpleTest) if defined?(SimpleTest)
+  def test_enqueue
+    fork do
+      Qutest.kestrel_queue('name') << Dir["#{TEST_DIR}/data/simple_test.rb"]
+
+      expected = {'name' => ["SimpleTest#test_failed", "SimpleTest#test_simple"]}
+      assert_equal(expected, Qutest.kestrel.store)
     end
   end
 
-  def test_enqueue
-    Qutest.kestrel_queue('name') << Dir["#{TEST_DIR}/data/simple_test.rb"]
-
-    expected = {'name' => ["SimpleTest#test_failed", "SimpleTest#test_simple"]}
-    assert_equal(expected, Qutest.kestrel.store)
-  end
-
   def test_dequeue
-    Qutest.kestrel_queue('name') << Dir["#{TEST_DIR}/data/simple_test.rb"]
-    result = Qutest.run(Qutest.kestrel_queue('name'), :name => :embedded)
+    fork do
+      Qutest.kestrel_queue('name') << Dir["#{TEST_DIR}/data/simple_test.rb"]
+      result = Qutest.run(Qutest.kestrel_queue('name'), :name => :embedded)
 
-    assert_equal "2 tests, 2 assertions, 1 failures, 0 errors", result.to_s
+      assert_equal "2 tests, 2 assertions, 1 failures, 0 errors", result.to_s
+    end
   end
 
   def test_pp_to_s
--- test/qutest_test.rb
+++ test/qutest_test.rb
@@ -4,62 +4,64 @@ require "qutest"
 class QutestTest < Test::Unit::TestCase
 
   def setup
-    GC.start
     @origin = []
     @queue = Qutest.queue(@origin)
   end
 
-  def teardown
-    Object.module_eval do
-      remove_const(:InModule)
-      remove_const(:SimpleTest)
-    end
-  end
-
   def test_put_all_tests_in_queue
-    @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
-    @queue.push Dir["#{TEST_DIR}/data/tests/*_test.rb"]
-    expected = [
-      "InModule::SimpleTest#test_failed",
-      "InModule::SimpleTest#test_simple",
-      "SimpleTest#test_failed",
-      "SimpleTest#test_simple",
-      "AnotherTest#test_another"
-    ]
-    assert_equal expected, @origin
+    fork do
+      @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
+      @queue.push Dir["#{TEST_DIR}/data/tests/*_test.rb"]
+      expected = [
+        "InModule::SimpleTest#test_failed",
+        "InModule::SimpleTest#test_simple",
+        "SimpleTest#test_failed",
+        "SimpleTest#test_simple",
+        "AnotherTest#test_another"
+      ]
+      assert_equal expected, @origin
+    end
   end
 
   def test_put_tests_in_queue_should_disable_auto_run
-    Test::Unit.run = false
-    @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
-    assert Test::Unit.run?
+    fork do
+      Test::Unit.run = false
+      @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
+      assert Test::Unit.run?
+    end
   end
 
   def test_run_tests_in_queue
-    @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
-    result = Qutest.run(@queue, :name => :embedded)
-    assert_equal [], @origin
-    assert_equal "4 tests, 4 assertions, 2 failures, 0 errors", result.to_s
+    fork do
+      @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
+      result = Qutest.run(@queue, :name => :embedded)
+      assert_equal [], @origin
+      assert_equal "4 tests, 4 assertions, 2 failures, 0 errors", result.to_s
+    end
   end
 
   def test_should_mark_test_unit_run_after_run_tests
-    @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
-    Test::Unit.run = false
-    result = Qutest.run(@queue, :name => :embedded)
-    assert Test::Unit.run?
+    fork do
+      @queue << Dir["#{TEST_DIR}/data/*_test.rb"]
+      Test::Unit.run = false
+      result = Qutest.run(@queue, :name => :embedded)
+      assert Test::Unit.run?
+    end
   end
 
   def test_should_be_able_to_monitor_enqueue_progress
-    progress = []
-    @queue.push Dir["#{TEST_DIR}/data/*_test.rb"] do |total, index, test|
-      progress << [total, index, test]
+    fork do
+      progress = []
+      @queue.push Dir["#{TEST_DIR}/data/*_test.rb"] do |total, index, test|
+        progress << [total, index, test]
+      end
+      expected = [
+        [4, 0, "InModule::SimpleTest#test_failed"],
+        [4, 1, "InModule::SimpleTest#test_simple"],
+        [4, 2, "SimpleTest#test_failed"],
+        [4, 3, "SimpleTest#test_simple"]
+      ]
+      assert_equal expected, progress
     end
-    expected = [
-      [4, 0, "InModule::SimpleTest#test_failed"],
-      [4, 1, "InModule::SimpleTest#test_simple"],
-      [4, 2, "SimpleTest#test_failed"],
-      [4, 3, "SimpleTest#test_simple"]
-    ]
-    assert_equal expected, progress
   end
 end
